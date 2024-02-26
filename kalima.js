a4fdcbb2f57b7c9a1dc4f2355ce1b6c1e314dd95
Kavinraj VP <kavinrajvp@gmail.com>
2024-02-26 22:53:30 +0530

    Removed Unwanted Lines

--- kalima.js
+++
@@ -1,6 +0,0 @@
-console.log('Im a Kalima');
-console.log('Im a Kavin');
-console.log('Kavin & Kalima');
-console.log('Im Kavin & Im Kalima');
-console.log("I'm Kavin & I'm Kalima");
-console.log("TESTING THE SQUASH");
