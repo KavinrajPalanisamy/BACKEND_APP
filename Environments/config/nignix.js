e82124177a5521a39cbffae45fac898e6f889aee
Kavinraj VP <kavinrajvp@gmail.com>
2024-10-20 21:02:10 +0530

    NGNIX ENV Added

---
+++ Environments/config/nignix.js
@@ -0,0 +1,10 @@
+var envConfig = {
+    HOST: '192.168.0.200',
+    USER: 'postgres',
+    PASSWORD: "1234",
+    DB: 'postgres',
+    SECRET_KEY: 'ffxr94jpvd',
+    PORT: '1200'
+}
+
+module.exports.envConfig = envConfig;
